f2e7f104d4135252af228e9f55ad5db44146172a
Filipe Silva <filipematossilva@gmail.com>
2019-12-14 12:07:53 +0000

    fix broken reagent usage

--- src/app/hello_cards.cljs
+++ src/app/hello_cards.cljs
@@ -1,11 +1,14 @@
 (ns app.hello-cards
-  (:require [devcards.core :as dc :refer [defcard defcard-rg]]
+  ; We shouldn't need to load reagent directly, but it looks like devcards
+  ; is importing it in such a way that it needs to be imported beforehand.
+  (:require [reagent.core]
+            [devcards.core :refer [defcard defcard-rg]]
             [app.hello :refer [add-ada]]
             [app.projections :refer [ds->seq]]
             [app.db :refer [fb-user-atom datascript-connection]]))
 
-(defcard add-ada-card
-  (dc/reagent add-ada))
+(defcard-rg add-ada-card
+  add-ada)
 
 (defcard firestore-user-collection-card
   (fb-user-atom) [] {:history false})
